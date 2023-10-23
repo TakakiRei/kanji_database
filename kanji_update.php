5f96aa2384a3004476e692a804f3eb1df48e1a30
TakakiRei <132850757+TakakiRei@users.noreply.github.com>
2023-10-23 15:33:18 +0900

    Create kanji_update.php

---
+++ kanji_update.php
@@ -0,0 +1,57 @@
+<HTML>
+<HEAD>
+  <TITLE>漢字データ更新処理スクリプト</TITLE>
+  <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</HEAD>
+<BODY>
+
+<!-- ここからPHPのスクリプト始まり -->
+<?php
+
+// フォームから渡された引数を取得
+$letter = $_GET[ letter ];
+$radical = $_GET[ radical ];
+$str_num = $_GET[ str_num ];
+$grade = $_GET[ grade ];
+
+// データベースに接続
+$conn = pg_connect( "dbname=akyw9019" );
+
+// 接続が成功したかどうか確認
+if ( $conn == null )
+{
+	print( "データベース接続処理でエラーが発生しました。<BR>" );
+	exit;
+}
+
+// データ更新のSQLを作成
+$sql = "update kanji set radical='$radical', str_num='$str_num', grade = '$grade' where letter = '$letter'";
+
+// 確認用のメッセージ表示
+print( "クエリー「" );
+print( $sql );
+print( "」を実行します。<BR>" );
+
+// Queryを実行して検索結果をresultに格納
+$result = pg_query( $conn, $sql );
+if ( $result == null )
+{
+	print( "クエリー実行処理でエラーが発生しました。<BR>" );
+	exit;
+}
+
+// 検索結果の開放
+pg_free_result( $result );
+
+// データベースへの接続を解除
+pg_close( $conn );
+
+?>
+<!-- ここまででPHPのスクリプト終わり -->
+
+データの更新処理が完了しました。<BR>
+<BR>
+<A HREF="kanji_menu.html">操作メニューに戻る</A>
+
+</BODY>
+</HTML>
